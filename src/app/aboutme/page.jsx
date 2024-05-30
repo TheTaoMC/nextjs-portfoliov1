d961984adc208c0891b3ad169e5d3bdec310869d
TheTaoMC <manamanote@gmail.com>
2024-05-31 00:26:58 +0700

    f3

--- src/app/aboutme/page.jsx
+++ src/app/aboutme/page.jsx
@@ -1,12 +1,13 @@
 import React from "react";
+import Container from "../components/container/Container";
 
 function AboutME() {
   return (
     <>
       <div
         style={{
-          
-          backgroundImage: "linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('/bg.avif')", 
+          backgroundImage:
+            "linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('/bg.avif')",
           backgroundSize: "cover",
           backgroundPosition: "center",
         }}
@@ -20,6 +21,47 @@ function AboutME() {
             </a>
           </div>
         </div>
+      </div>
+
+      <div>
+        <Container>
+          <div className="text-center py-4 text-2xl font-bold">ABOUT ME</div>
+          <hr />
+          <div className="flex">
+            <div className="w-1/2">
+              <div>
+                I am a Professional UI/UX Designer and Web developer.
+                Consectetur an adipisi elita, sed do eiusmod tempor incididunt
+                ut labore et dolore magna aliqua. Ut enim ad minim veniam quis
+                nostrud.
+              </div>
+              <div>
+                Duis aute irure dolor in reprehenderit in voluptate velit esse
+                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                cupidatat non proident, sunt in culpa qui officia deserunt
+                mollit anim id est laborum. Sed ut perspi unde omnis iste natus
+                error sit voluptatem accusantium doloremque lauda ntium, totam
+                rem aperiam, eaque ipsa quae ab illo inventore veritatis et
+                quasi architecto beatae vitae dicta sunt explicabo. Nemo enim
+                ipsam vo luptatem quia voluptas sit aspernatur aut odit aut
+                fugit,
+              </div>
+              <hr />
+              <div className="flex justify-between">
+                <div>Phone 123-456-7890</div>
+                <div>Email name@domain.com</div>
+                <div>Website www.domain.com</div>
+              </div>
+            </div>
+
+            <div className="w-1/2">
+              <div className="flex flex-col">
+                <div className="bg-black w-full h-28"></div>
+                <div className="bg-black w-full h-28"></div>
+              </div>
+            </div>
+          </div>
+        </Container>
       </div>
     </>
   );
